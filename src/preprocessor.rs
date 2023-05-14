5c15fc8d3cdc82a93596a57211e09ff0eb5ede3b
nonowar7 <sabalanoam77@gmail.com>
2023-05-14 18:20:33 +0300

    preprocessor.rs first version

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -23,46 +23,44 @@ mod preprocessor {
             *sentences = reduced_sentences;
         }
 
-        // Counter is treated as a hash map.
-        // rust strings are vectors, thus there should not be a problem to join X sentences to one variable of 
-        // type String, if the first one fits in memory. In other words, if we were able to load X sentences
-        // into a Vec<String>, those should fit in one chunk. 
-        fn count_tokens(&self, sentences: &Vec<String>) -> Counter<String, i8> {
-            let token_chunk = sentences.join(" ");
-            let token2count: Counter<String, i8> = token_chunk.split_whitespace().map(|x| x.to_string()).collect();
-            token2count
-        }
-
         // uses the counter to get a vector of unique chars
-        fn count_chars(&self, vocab: &Vec<String>) -> Counter<char, i8> {
+        fn count_chars(&self, vocab: &Vec<String>, char_start: char, char_end: char) -> Counter<char, i8> {
             let char_chunk = vocab.join("");
-            let char2count: Counter<char, i8> = char_chunk.chars().collect();
+            let mut char2count: Counter<char, i8> = char_chunk.chars().collect();
+            let n = char2count.len();
+            char2count.extend([(char_start, n as i8), (char_end, 1 + n as i8)]);
             char2count
         }
 
-        fn preprocess(&mut self, sentences: &mut Vec<String>, vocab_size: usize) -> (HashMap<String, u8>, HashMap<String, u8>){
+        fn preprocess(&mut self, sentences: &mut Vec<String>, vocab_size: usize) -> HashMap<char, u8>{
 
             // strip duplicated sentences
             self.unique(sentences);
 
-            // create vocabulary of vocab_size most common tokens
-            let token2count = self.count_tokens(sentences.as_ref());
-            let mut vocab: Vec<String> = token2count.most_common_ordered()
-            .into_iter().map(|(s, _)| s).collect::<Vec<String>>();
-            vocab.truncate(vocab_size);
-
-            // add SOS, EOS and UNK to vocab
-            vocab.append(&mut vec!["<SOS>", "<EOS>", "<UNK>"].into_iter().map(|x| x.to_string()).collect_vec());
-            let token2int: HashMap<String, u8> = vocab.iter().enumerate().map(|(i, t)| (t.to_string(), i as u8)).collect();
+            // pad sentences with SOS + EOS
+            let char_start = '$';
+            let char_end = '^';
+            sentences.iter_mut().map(|s| { // filtering future EOT and SOT chars
+                *s = s.trim_matches(' ').to_string(); // remove leading and trailing spaces
+                *s = s.chars().filter(|x| x != &char_start && x != &char_end).collect::<String>();
+                *s = "SOS ".to_string() + s;
+                *s += " EOS";
+            });
 
             // crate vocabulary of chars
-            let char2count = self.count_chars(&vocab);
+            let char2count = self.count_chars(&sentences, char_start, char_end);
             let chars = char2count.into_iter().map(|(c, _)| c).collect::<Vec<char>>();
-            let char2int: HashMap<String, u8> = chars.iter().enumerate().map(|(i, c)| (c.to_string(), i as u8)).collect();
+            let char2int: HashMap<char, u8> = chars
+            .iter()
+            .enumerate()
+            .map(|(i, c)| (c.to_owned(), i as u8))
+            .collect();
 
-            // replace words not in voabulary or under 3 occurrences with <unk>, pad with SOS + EOS
-            // will happen in the loader stages, with random shuffling for each iteration (pytorch style)
-            (token2int, char2int)
+            // char2int has all the chars in the corpus + start + end chars, that has been filtered from the sentences.
+            // sentences don't have duplications, padded with SOS and EOS tokens.
+            // there is no UNK for chars, an unknown char will be replaced with it's uft8 encoding.
+
+            char2int
         }
 
     }
