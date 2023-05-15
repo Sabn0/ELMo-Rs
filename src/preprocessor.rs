c6afd63afeb611ed907fc68525ac88b453ce11a0
nonowar7 <sabalanoam77@gmail.com>
2023-05-15 13:19:22 +0300

    preprocessor.rs with token2int

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -11,6 +11,34 @@ mod preprocessor {
 
     use counter::Counter;
     use itertools::Itertools;
+
+    pub trait CollectT {
+        type Item;
+        fn collect_gen(elements: Vec<Self::Item>) -> HashMap<Self::Item, u8>;
+    }
+    impl CollectT for char {
+        type Item = Self;
+        fn collect_gen(elements: Vec<Self::Item>) -> HashMap<Self::Item, u8> {
+            let x: HashMap<char, u8> = elements.
+            into_iter()
+            .enumerate()
+            .map(|(i, e)| (e, i as u8))
+            .collect();
+            x
+        }
+    }
+    impl CollectT for String {
+        type Item = Self;
+        fn collect_gen(elements: Vec<Self::Item>) -> HashMap<Self::Item, u8> {
+            let x: HashMap<String, u8> = elements.
+            into_iter()
+            .enumerate()
+            .map(|(i, e)| (e, i as u8))
+            .collect();
+            x
+        }
+    }
+
     pub(in crate) struct Preprocessor {}
 
     impl Preprocessor {
@@ -23,16 +51,31 @@ mod preprocessor {
             *sentences = reduced_sentences;
         }
 
+        // uses the counter to get a vocabulary of words
+        fn count_tokens(&self, sentences: &Vec<String>, vocab_size: usize, min_count: usize) -> Vec<String> {
+            let chunk = sentences.join(" ");
+            let token2count: Counter<String, i8> = chunk.split_whitespace().map(|x| x.to_string()).collect();
+            let tokens = token2count.k_most_common_ordered(vocab_size);
+            let tokens = tokens
+            .into_iter()
+            .filter(|(_, c)| *c as usize >= min_count)
+            .map(|(t, _)| t)
+            .collect::<Vec<String>>();
+            tokens
+        }
+
         // uses the counter to get a vector of unique chars
-        fn count_chars(&self, vocab: &Vec<String>, char_start: char, char_end: char) -> Counter<char, i8> {
+        fn count_chars(&self, vocab: &Vec<String>, char_start: char, char_end: char) -> Vec<char> {
             let char_chunk = vocab.join("");
             let mut char2count: Counter<char, i8> = char_chunk.chars().collect();
             let n = char2count.len();
             char2count.extend([(char_start, n as i8), (char_end, 1 + n as i8)]);
-            char2count
+            let chars = char2count.into_iter().map(|(c, _)| c).collect::<Vec<char>>();
+            chars
         }
 
-        fn preprocess(&mut self, sentences: &mut Vec<String>, vocab_size: usize) -> HashMap<char, u8>{
+
+        fn preprocess(&mut self, sentences: &mut Vec<String>, vocab_size: usize, min_count: usize) -> (HashMap<String, u8>, HashMap<char, u8>) {
 
             // strip duplicated sentences
             self.unique(sentences);
@@ -40,27 +83,26 @@ mod preprocessor {
             // pad sentences with SOS + EOS
             let char_start = '$';
             let char_end = '^';
-            sentences.iter_mut().map(|s| { // filtering future EOT and SOT chars
+            sentences.iter_mut().for_each(|s| { // filtering future EOT and SOT chars
                 *s = s.trim_matches(' ').to_string(); // remove leading and trailing spaces
                 *s = s.chars().filter(|x| x != &char_start && x != &char_end).collect::<String>();
                 *s = "SOS ".to_string() + s;
                 *s += " EOS";
             });
 
-            // crate vocabulary of chars
-            let char2count = self.count_chars(&sentences, char_start, char_end);
-            let chars = char2count.into_iter().map(|(c, _)| c).collect::<Vec<char>>();
-            let char2int: HashMap<char, u8> = chars
-            .iter()
-            .enumerate()
-            .map(|(i, c)| (c.to_owned(), i as u8))
-            .collect();
+            // create vocabulary of words
+            let tokens = self.count_tokens(&sentences, vocab_size, min_count);
+            let token2int: HashMap<String, u8> = <String as CollectT>::collect_gen(tokens);
+
+            // create vocabulary of chars
+            let chars = self.count_chars(&sentences, char_start, char_end);
+            let char2int: HashMap<char, u8> = <char as CollectT>::collect_gen(chars);
 
             // char2int has all the chars in the corpus + start + end chars, that has been filtered from the sentences.
             // sentences don't have duplications, padded with SOS and EOS tokens.
             // there is no UNK for chars, an unknown char will be replaced with it's uft8 encoding.
 
-            char2int
+            (token2int, char2int)
         }
 
     }
